35c9171afae16dcfc0d6fec7332634c8d42ba184
Heverton Eduardo Peres <hevertoneduardoperes@gmail.com>
2026-07-31 17:33:50 -0300

    fix: remover crossorigin do build para CSS carregar no Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function removeCrossorigin() {
+  return {
+    name: 'remove-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/ crossorigin/g, '')
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeCrossorigin()],
+  build: {
+    modulePreload: { polyfill: false },
+  },
 })
